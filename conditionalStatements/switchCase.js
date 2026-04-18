af00d0dc32c789e785fb09d60d20bf0626f59e74
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2026-04-18 14:44:41 -0400

    applying best practices switch case

---
+++ conditionalStatements/switchCase.js
@@ -0,0 +1,72 @@
+const daysWeek = 6;
+
+switch (daysWeek) {
+  case 1:
+    console.log("Hoje é Domingo");
+    break;
+  case 2:
+    console.log("Hoje é Segunda-Feira");
+    break;
+  case 3:
+    console.log("Hoje é Terça-Feira");
+    break;
+  case 4:
+    console.log("Hoje é Quarta-Feira");
+    break;
+  case 5:
+    console.log("Hoje é Quinta-Feira");
+    break;
+  case 6:
+    console.log("Hoje é Sexta-Feira");
+    break;
+  case 7:
+    console.log("Hoje é Sabado");
+    break;
+
+  default:
+    break;
+}
+
+const monthYear = 11;
+
+switch (monthYear) {
+  case 1:
+    console.log("Janeiro");
+    break;
+  case 2:
+    console.log("Fevereiro");
+    break;
+  case 3:
+    console.log("Março");
+    break;
+  case 4:
+    console.log("abril");
+    break;
+  case 5:
+    console.log("Maio");
+    break;
+  case 6:
+    console.log("Junho");
+    break;
+  case 7:
+    console.log("julho");
+    break;
+  case 8:
+    console.log("Agosto");
+    break;
+  case 9:
+    console.log("Setembro");
+    break;
+  case 10:
+    console.log("Outubro");
+    break;
+  case 11:
+    console.log("Novembro");
+    break;
+  case 12:
+    console.log("Dezembro");
+    break;
+  default:
+    console.log("Mês incorreto escolha de 1 a 12");
+    break;
+}
