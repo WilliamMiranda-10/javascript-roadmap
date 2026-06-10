5dc6da5a90e7d37c2bdceb919dd759509fb2bae9
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2026-06-09 21:49:56 -0400

    studying function parameters

--- expressionsOperators/stringOperators.js
+++ expressionsOperators/stringOperators.js
@@ -1,14 +1,15 @@
 //ex: 01
 
-let name = 'William' + 'Miranda' // concatena as strings
+let name = "William" + "Miranda"; // concatena as strings
 
-console.log(name) // WilliamMiranda
-console.log(typeof name) // string
+console.log(name); // WilliamMiranda
+console.log(typeof name); // string
 
 //ex: 02
-let numberString = 15 + "10"
+let numberString = 15 + "10";
 
-console.log(numberString) // 1510
-console.log(typeof numberString) //string
+console.log(numberString); // 1510
+console.log(typeof numberString); //string
 
-
+let result = 2 + 2 + "4"; //soma os dois valores (2+2) e concatena com a string "4"
+console.log(result); //44
---
+++ functions/defaultParams.js
@@ -0,0 +1,34 @@
+function multiply(a, b) {
+  b = typeof b !== "undefined" ? b : 5;
+  // o typeof de b e diferente de undefined ? se for true b recebe 5
+
+  return a * b;
+}
+
+console.log(multiply(10, 9));
+
+function sum(a, b = 150) {
+  return a + b;
+}
+
+console.log(sum(10)); //160
+
+function hello(nome) {
+  console.log("Olá", nome);
+}
+
+hello(); //Olá undefined --> como não passei nenhum valor para o paramentro nome o resultado deu undefined para nome
+hello("William"); // Olá William
+
+function createUser(name, age = 30) {
+  console.log(name, age);
+}
+
+createUser("William"); // William 30
+
+function applyDiscount(value, discount = 5) {
+  return value - (value * discount) / 100;
+}
+// Valor padrao (discount = 5) e usado quando nenhum argumento for passado
+console.log(applyDiscount(15000)); // 14250
+console.log(applyDiscount(4000, 10)); //3600
