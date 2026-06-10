d8873352db9a2daddbaed2d80b46314bf7ad6337
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2026-06-10 19:43:18 -0400

    studying function parameters

--- functions/functionParameters.js
+++ functions/functionParameters.js
@@ -1,3 +1,5 @@
+// Default parameters -> Parâmetros padrão
+
 function multiply(a, b) {
   b = typeof b !== "undefined" ? b : 5;
   // o typeof de b e diferente de undefined ? se for true b recebe 5
@@ -32,3 +34,26 @@ function applyDiscount(value, discount = 5) {
 // Valor padrao (discount = 5) e usado quando nenhum argumento for passado
 console.log(applyDiscount(15000)); // 14250
 console.log(applyDiscount(4000, 10)); //3600
+
+// Rest parameters ->
+
+function puchaseValue(discount, ...puchases) {
+  let totalValue = 0;
+
+  for (value of puchases) {
+    totalValue += value;
+  }
+  let valueDiscount = totalValue - (totalValue / 100 * discount )
+
+  return `Valor total: R$ ${valueDiscount.toFixed(2)}`;
+}
+
+console.log(puchaseValue(5, 15, 6.5, 19.99, 7.49, 4.99, 5, 6.45));
+
+
+function fruitSalad(aliment,...fruits){
+   console.log('Alimentos:', aliment)
+   console.log(fruits)
+}
+
+fruitSalad('Arroz', 'manga', 'jaca', 'laranja')
