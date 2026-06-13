8e76d865752ba78cc3ad6181bceaa47fb4e30313
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2026-06-13 16:07:30 -0400

    study: learning built-in-functions

---
+++ functions/builtInFunctions.js
@@ -0,0 +1,22 @@
+// built-in-functions --> funções ja prontas
+
+//Exemplo 01:
+
+console.log("Hello World")
+
+// Exemplo 02
+
+const x = String(100) // string é função pronta
+console.log(typeof x) //String
+
+// Exemplo 03
+
+setTimeout(()=>{
+    console.log('Execultado após 1 segundo')
+}, 1000)
+
+setInterval(()=>{
+    console.log('Execulta a  cada 2 segundos')
+}, 2000) 
+
+
