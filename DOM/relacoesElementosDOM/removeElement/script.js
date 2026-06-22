0bb2da711b6464d5422462a5d64a69bc720212e6
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2026-06-22 09:57:28 -0400

    study: adding comments

--- DOM/relacoesElementosDOM/removeElement/script.js
+++ DOM/relacoesElementosDOM/removeElement/script.js
@@ -8,20 +8,20 @@ console.log(caixa1.children); // todos elementos
 const cursos = ["HTML", "CSS", "Javascript", "PHP", "React", "MySql"];
 
 cursos.map((curso, chave) => {
-  const novoElemento = document.createElement("div");
+  const novoElemento = document.createElement("div"); //cria o elemento div
   novoElemento.innerHTML = curso;
-  novoElemento.setAttribute("id", "c" + chave);
-  novoElemento.setAttribute("class", "curso c1");
+  novoElemento.setAttribute("id", "c" + chave); // adidiciona um id
+  novoElemento.setAttribute("class", "curso c1"); // adiciona uma class
 
-  const btn_lixeira = document.createElement("img");
-  btn_lixeira.setAttribute("src", "./lixeira.png");
-  btn_lixeira.setAttribute("class", "lixeira");
+  const btn_lixeira = document.createElement("img"); // cria uma img como button
+  btn_lixeira.setAttribute("src", "./lixeira.png"); // adiciona o src o caminho da img
+  btn_lixeira.setAttribute("class", "lixeira"); // adiciona uma classe
+
+  novoElemento.appendChild(btn_lixeira); // adiciona a lixeira dentro de cada div dos cursos que foi criada logo acima
+  caixa1.appendChild(novoElemento); // adiciona as div dos cursos que foi criado dentro da caixa1
 
   btn_lixeira.addEventListener("click", (evt) => {
     console.log(evt.target.parentNode);
-    caixa1.removeChild(evt.target.parentNode);
+    caixa1.removeChild(evt.target.parentNode); // remove filhos da caixa1 ou seija os elementos div com cursos
   });
-
-  novoElemento.appendChild(btn_lixeira);
-  caixa1.appendChild(novoElemento);
 });
