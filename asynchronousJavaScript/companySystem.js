9943b604cbea62552b8c793fdceb1ac6ad0ba1e1
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2026-06-29 19:48:45 -0400

    study: practice promise.all

--- asynchronousJavaScript/companySystem.js
+++ asynchronousJavaScript/companySystem.js
@@ -2,7 +2,7 @@
 const getCompany = (id) => {
   return new Promise((resolve, reject) => {
     const companyData = {
-      id: 2,
+      id: 1,
       nome: "Império tech",
     };
 
@@ -55,12 +55,12 @@ const getEmployees = (companyId) => {
       }
 
       resolve(companyEmploeeId);
-    });
-  }, 1000);
+    }, 1000);
+  });
 };
 
 const getEmployeeProjects = (employeeName) => {
-  new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const employeeProjectsData = {
       William: [
         { id: 101, nome: "API financeira" },
@@ -84,12 +84,12 @@ const getEmployeeProjects = (employeeName) => {
       );
 
       resolve(employeeProjectsData[employeeName]);
-    });
-  }, 1000);
+    }, 1000);
+  });
 };
 
 const getProjectTasks = (projectId) => {
-  new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const tasksProjectData = {
       101: [
         { id: 1, titulo: "Criar rota" },
@@ -107,7 +107,7 @@ const getProjectTasks = (projectId) => {
           titulo: "Criar gráficos do dashboard ",
         },
       ],
-      301: [{ id: 6, titulo: "Criar testes unitários" }],
+      301: [{ id: 7, titulo: "Criar testes unitários" }],
       302: [
         { id: 8, titulo: "Testar aplicação em dispositivos móveis" },
         { id: 9, titulo: "Testar responsividade" },
@@ -121,26 +121,36 @@ const getProjectTasks = (projectId) => {
 };
 
 const result = async () => {
-  const company = await getCompany(2);
+  const company = await getCompany(1);
   console.log("Company:", company);
   const employees = await getEmployees(company);
-  const employeeName = employees.map((name)=> getEmployeeProjects(name.nome))
+  console.log("employee:", employees);
+  const promiseProjects = employees.map((name) =>
+    getEmployeeProjects(name.nome)
+  );
+  console.log("promiseProject:", promiseProjects); // uma promise que contem um array de promise
+  const listProjects = await Promise.all(promiseProjects);
+  console.log("listprojects", listProjects); // É um array de arrays de objetos que contem projetos
 
-  console.log("Employees:", employees);
-  console.log("Employees Name:", employeeName);
+  const promiseTasks = listProjects.map(async (arrayProjects) => {
+    const grupoPromises = arrayProjects.map((project, index) => {
+      return getProjectTasks(project.id);
+    });
+    const resu = await Promise.all(grupoPromises);
+    console.log('resu',resu)
+    return {
+      funcionario: employees[index].nome,
+      cargo: employees[index].cargo,
+      projetos: arrayProjects[index]
+    };
+  });
 
-  
+  const resultado = await Promise.all(promiseTasks);
 
-  
+  console.log('resultado :',resultado)
+  // // console.log('resultado :',resultado[0][0])
+  // console.log('resultado :',resultado[0][0][0])
+  // console.log(JSON.stringify(resultado, null, 2));
 };
 
 result();
-
-
-
-
-
-
-
-
-
