99903d8c553153d5df30c04f30373c44790b1d35
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2026-07-10 22:00:58 -0400

    study: practice destructuring

--- destructuring/index.js
+++ destructuring/index.js
@@ -1,7 +1,7 @@
-const product = {
-  name: "Notebook",
-  price: 3500,
-};
+// const product = {
+//   name: "Notebook",
+//   price: 3500,
+// };
 
 // const { name: productName } = product;
 // const { price: productPrice } = product;
@@ -13,7 +13,7 @@ const product = {
 // Notebook
 // 3500
 
-const { name, price: productPrice, stock = 0 } = product;
+// const { name, price: productPrice, stock = 0 } = product;
 
 console.log(name);
 console.log(productPrice);
@@ -24,10 +24,57 @@ const colors = ["red", "blue", "green"];
 // const [a, b, c] = colors;
 // console.log(a, b, c);
 
-const numbers = [10, 20, 30, 40];
+// const numbers = [10, 20, 30, 40];
 
-const [a, b, c] = numbers;
+// const [a, b, c] = numbers;
 
 // console.log(a);
 // console.log(b);
 console.log(c);
+
+function getUser() {
+  return ["William", 30, "Backend"];
+}
+
+const [name, age, profession] = getUser();
+
+console.log(name);
+console.log(age);
+console.log(profession);
+
+function getProduct() {
+  return ["Notebook", 3500, 10, "Dell"];
+}
+
+const [product, , , marca] = getProduct();
+
+console.log(product);
+// console.log(marca);
+
+// const numbers = [10, 20, 30, 40, 50];
+// const [first, ...rest] = numbers;
+
+console.log(first, rest); //10 [ 20, 30, 40, 50 ]
+
+
+const fruits = ["Maçã", "Banana"];
+
+const moreFruits = [...fruits, "Laranja"];
+
+console.log(moreFruits)
+
+
+
+const user = {
+  name: "William",
+  age: 30,
+};
+
+const updatedUser = {
+  ...user,
+  age: 31,
+  city: "Jaru",
+};
+
+console.log(updatedUser);
+
